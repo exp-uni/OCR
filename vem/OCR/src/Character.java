6c720e7457ac44fc42d9343888f7eb4b2cd29c50
Dennis <dwess@github.com>
2015-11-03 18:44:01 -0500

    Forgot to add Character.java. Currently holds an array of boolean segments, used to identify the character

---
+++ vem/OCR/src/Character.java
@@ -0,0 +1,20 @@
+package OCR;
+
+import java.util.ArrayList;
+
+/**
+ * Holds boolean values identifying each segment
+ *
+ */
+public class Character {
+
+	
+	ArrayList<Segment> segments;
+	public Character(){
+		segments = new ArrayList<Segment>();
+	}
+	
+	public void addSegment(Segment seg){
+		segments.add(seg);
+	}
+}
